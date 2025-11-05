0bdd24b4898131eecb58f195815ad86ac380979c
Benjamin Speake <benjaminspeake@gmail.com>
2025-11-05 15:33:12 +0000

    Add output fields to the base calculation for storing the (optional) trajectory files produced during geometry optimisation

--- src/aiida_chemshell/calculations/base.py
+++ src/aiida_chemshell/calculations/base.py
@@ -132,6 +132,20 @@ class ChemShellCalculation(CalcJob):
             ),
         )
 
+        spec.output(
+            "trajectory_path",
+            valid_type=SinglefileData,
+            required=False,
+            help="XYZ trajectory file for the geometry optimisation",
+        )
+        spec.output(
+            "trajectory_force",
+            valid_type=SinglefileData,
+            required=False,
+            help="XYZ trajectory containing forces at each step of a geometry "
+            "optimisation",
+        )
+
         ## Metadata
         spec.inputs["metadata"]["options"]["resources"].default = {
             "num_machines": 1,
@@ -822,5 +836,8 @@ class ChemShellCalculation(CalcJob):
         # file containing the optimised structure
         if "optimisation_parameters" in self.inputs:
             calc_info.retrieve_list.append(ChemShellCalculation.FILE_DLFIND)
+            if self.inputs.optimisation_parameters.get("save_path", False):
+                calc_info.retrieve_list.append(ChemShellCalculation.FILE_TRJPTH)
+                calc_info.retrieve_list.append(ChemShellCalculation.FILE_TRJFRC)
 
         return calc_info
